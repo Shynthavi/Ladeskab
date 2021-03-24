958a9967bfeb0ebf546e46e248d9bec731861f8f
LucasFriis <201811527@post.au.dk>
2021-03-24 10:45:00 +0100

    Tilføjet RfidEventSimulator + RfidReader Interface

---
+++ Ladeskabsystem/IRfidReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskabsystem
+{
+    public class RfidEventArgs : EventArgs
+    {
+        public bool isDetected { get; set; }
+    }
+
+    public interface IRfidReader
+    {
+        event EventHandler<RfidEventArgs> RfidEvent;
+    }
+}
---
+++ Ladeskabsystem/RfidReaderSimulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Ladeskabsystem
+{
+    public class RfidReaderSimulator : IRfidReader
+    {
+        public event EventHandler<RfidEventArgs> RfidEvent;
+        public void OnNewRfid(RfidEventArgs e)
+        {
+            RfidEvent?.Invoke(this, e);
+        }
+
+    }
+}
