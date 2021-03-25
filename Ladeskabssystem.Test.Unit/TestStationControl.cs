c61c90de84182a480172723781ed2999d26ce6ad
Henry Pham <HenryPham@live.dk>
2021-03-25 16:41:42 +0100

    added TestStationControl

---
+++ Ladeskabssystem.Test.Unit/TestStationControl.cs
@@ -0,0 +1,21 @@
+﻿using Ladeskabsystem;
+using Ladeskabsystem.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Ladeskabssystem.Test.Unit
+{
+    class TestStationControl
+    {
+        private StationControl _uut;
+        private IUsbCharger _charger;
+        private Read
+
+        [SetUp]
+        public void Setup()
+        {
+            _charger = Substitute.For<IUsbCharger>();
+            _uut = new ChargeControl(_charger);
+        }
+    }
+}
