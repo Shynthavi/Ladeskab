ad385a92261f5f0a9e16d2d56b776d21970748f3
LucasFriis <201811527@post.au.dk>
2021-03-25 17:03:40 +0100

    Tilføjet test til LogFileTest

--- Ladeskabssystem.Test.Unit/TestLogFile.cs
+++ Ladeskabssystem.Test.Unit/TestLogFile.cs
@@ -9,35 +9,63 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using NSubstitute.ReceivedExtensions;
 
 
 namespace Ladeskabssystem.Test.Unit
 {
+     
+
     [TestFixture]
     public class TestLogFile
     {
         private LogFile _uut;
         private string Logfile = "Logfile.txt";
+        private RfidReaderSimulator _rfidReader;
+        private IDoor _iDoor;
+        private IChargeControl _chargeControl;
+        private IDisplay _iDisplay;
+        private StationControl _stationControl;
 
         [SetUp]
         public void Setup()
         {
             _uut = new LogFile();
+            _rfidReader = new RfidReaderSimulator();
+            _iDoor = Substitute.For<IDoor>();
+            _chargeControl = Substitute.For<IChargeControl>();
+            _iDisplay = Substitute.For<IDisplay>();
+            _stationControl = new StationControl(_chargeControl, _iDoor, _iDisplay, _rfidReader);
+        }
+
+        [Test]
+        public void LogDoorLock()
+        {
+            int LogLength = File.ReadAllText(@"C:\Users\Lucas\Desktop\Aarhus_Universitet\IKT\4. Semester\SWT\Github\Ladeskab\Ladeskabssystem.Test.Unit\bin\Debug\netcoreapp3.1\logfile.txt").Length;
+            int Id = 14;
+
+            _chargeControl.IsConnected().Returns(true);
+            _rfidReader.SimulateReading(Id);
             
+            int newLogLength = File.ReadAllText(@"C:\Users\Lucas\Desktop\Aarhus_Universitet\IKT\4. Semester\SWT\Github\Ladeskab\Ladeskabssystem.Test.Unit\bin\Debug\netcoreapp3.1\logfile.txt").Length;
+            Assert.That(newLogLength,Is.GreaterThan(LogLength));
+
         }
 
-        [Test]
-        public void LogDoorIsLocked(int id)
-        {
-            _uut.LogDoorLocked(id);
-            string [] loadfile = File.ReadAllLines(Logfile + "Logfile.txt");
-            Assert.That(loadfile[loadfile.Length - 1], Is.EqualTo("Door locked " + id));
-        }
 
         [Test]
-        public void LogDoorIsUnlocked(int id)
+        public void LogDoorUnlock()
         {
-            _uut.LogDoorUnlocked(id);
+            int Id = 14;
+
+            _chargeControl.IsConnected().Returns(true);
+            int LogLength = File.ReadAllText(@"C:\Users\Lucas\Desktop\Aarhus_Universitet\IKT\4. Semester\SWT\Github\Ladeskab\Ladeskabssystem.Test.Unit\bin\Debug\netcoreapp3.1\logfile.txt").Length;
+            _rfidReader.SimulateReading(Id);
+
+            int newLogLength = File.ReadAllText(@"C:\Users\Lucas\Desktop\Aarhus_Universitet\IKT\4. Semester\SWT\Github\Ladeskab\Ladeskabssystem.Test.Unit\bin\Debug\netcoreapp3.1\logfile.txt").Length;
+            Assert.That(newLogLength, Is.GreaterThan(LogLength));
+
         }
+
     }
 }
--- Ladeskabssystem.Test.Unit/TestStationControl.cs
+++ Ladeskabssystem.Test.Unit/TestStationControl.cs
@@ -5,17 +5,17 @@ using NUnit.Framework;
 
 namespace Ladeskabssystem.Test.Unit
 {
-    class TestStationControl
-    {
-        private StationControl _uut;
-        private IUsbCharger _charger;
-        private Read
+    //class TestStationControl
+    //{
+    //    private StationControl _uut;
+    //    private IUsbCharger _charger;
+    //    private Read
 
-        [SetUp]
-        public void Setup()
-        {
-            _charger = Substitute.For<IUsbCharger>();
-            _uut = new ChargeControl(_charger);
-        }
-    }
+    //    [SetUp]
+    //    public void Setup()
+    //    {
+    //        _charger = Substitute.For<IUsbCharger>();
+    //        _uut = new ChargeControl(_charger);
+    //    }
+    //}
 }
