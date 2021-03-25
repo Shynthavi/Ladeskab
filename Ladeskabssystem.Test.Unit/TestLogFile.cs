40b693067c86c24a18b977d67af1ec9b082c1b22
Henry Pham <HenryPham@live.dk>
2021-03-25 20:52:42 +0100

    changed logfile tests

--- Ladeskabssystem.Test.Unit/TestLogFile.cs
+++ Ladeskabssystem.Test.Unit/TestLogFile.cs
@@ -20,58 +20,39 @@ namespace Ladeskabssystem.Test.Unit
     public class TestLogFile
     {
         private LogFile _uut;
-        //private string Logfile = "Logfile.txt";
-        private RfidReaderSimulator _rfidReader;
-        private IDoor _iDoor;
-        private IChargeControl _chargeControl;
-        private IDisplay _iDisplay;
-        private StationControl _stationControl;
 
         [SetUp]
         public void Setup()
         {
             _uut = new LogFile();
-            _rfidReader = new RfidReaderSimulator();
-            _iDoor = Substitute.For<IDoor>();
-            _chargeControl = Substitute.For<IChargeControl>();
-            _iDisplay = Substitute.For<IDisplay>();
-            _stationControl = new StationControl(_chargeControl, _iDoor, _iDisplay, _rfidReader, _uut);
         }
 
         [Test]
         public void LogDoorLock()
         {
-            int Id = 14;
-            _rfidReader.SimulateReading(Id);
+            int id = 5;
             string path = Directory.GetCurrentDirectory();
             int LogLength = File.ReadAllText(path+@"\logfile.txt").Length;
             
-
-            _chargeControl.IsConnected().Returns(true);
-            _rfidReader.SimulateReading(Id);
+            _uut.LogDoorLocked(5);
 
             int newLogLength = File.ReadAllText(path + @"\logfile.txt").Length;
             Assert.That(newLogLength, Is.GreaterThan(LogLength));
 
         }
-        
 
         [Test]
         public void LogDoorUnlock()
         {
-            int Id = 14;
-            _rfidReader.SimulateReading(Id);
+            int id = 14;
             string path = Directory.GetCurrentDirectory();
             
-
-            _chargeControl.IsConnected().Returns(true);
-            
-            _rfidReader.SimulateReading(Id);
             int LogLength = File.ReadAllText(path+@"\logfile.txt").Length;
-          _rfidReader.SimulateReading(Id);
+
+            _uut.LogDoorUnlocked(id);
+
             int newLogLength = File.ReadAllText(path + @"\logfile.txt").Length;
             Assert.That(newLogLength, Is.GreaterThan(LogLength));
-
         }
 
     }
--- Ladeskabssystem.Test.Unit/TestStationControl.cs
+++ Ladeskabssystem.Test.Unit/TestStationControl.cs
@@ -5,17 +5,28 @@ using NUnit.Framework;
 
 namespace Ladeskabssystem.Test.Unit
 {
-    //class TestStationControl
-    //{
-    //    private StationControl _uut;
-    //    private IUsbCharger _charger;
-    //    private Read
+    class TestStationControl
+    {
+        private StationControl _uut;
+        private IUsbCharger _charger;
+        private IRfidReader _reader;
+        private IDoor _door;
+        private IChargeControl _chargeControl;
+        private IDisplay _display;
+        private ILogFile _log;
 
-    //    [SetUp]
-    //    public void Setup()
-    //    {
-    //        _charger = Substitute.For<IUsbCharger>();
-    //        _uut = new ChargeControl(_charger);
-    //    }
-    //}
+        [SetUp]
+        public void Setup()
+        {
+            _door = Substitute.For<IDoor>();
+            _reader = Substitute.For<IRfidReader>();
+            _chargeControl = Substitute.For<IChargeControl>();
+            _display = Substitute.For<IDisplay>();
+            _log = Substitute.For<ILogFile>();
+            _uut = new StationControl(_chargeControl, _door, _display, _reader, _log);
+        }
+
+
+
+    }
 }
