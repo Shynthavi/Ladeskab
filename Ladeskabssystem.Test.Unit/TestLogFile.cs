b55307e1831320a1db63267f14d3d797f19fad55
LucasFriis <201811527@post.au.dk>
2021-03-25 14:29:15 +0100

    Tilføjet test af LogFile

---
+++ Ladeskabssystem.Test.Unit/TestLogFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ladeskabsystem;
+using Ladeskabsystem.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Ladeskabssystem.Test.Unit
+{
+    [TestFixture]
+    public class TestLogFile
+    {
+        private LogFile _uut;
+        private ILogFile _ilogfile;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new LogFile();
+            
+        }
+
+        [Test]
+        public void IdSavedInLog_true()
+        {
+            Assert.That(()=>_uut.LogDoorLocked(7),Is.Not.Null);
+        }
+
+        [Test]
+        public void IdSavedInLog_false()
+        {
+            Assert.That(()=>_uut.LogDoorUnlocked(7),Is.Not.Null);
+        }
+
+    }
+}
