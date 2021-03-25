0f4b7773451f9138aa1aaf6d698a99edc1f3a9be
Shynthavi <57789965+Shynthavi@users.noreply.github.com>
2021-03-25 12:05:58 +0100

    LogFile

---
+++ Ladeskabsystem/Interfaces/ILogFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ladeskabsystem.Interfaces
+{
+    public interface ILogFile
+    {
+        void LogDoorLocked(int id);
+        void LogDoorUnlocked(int id);
+    }
+}
--- Ladeskabsystem/LogFile.cs
+++ Ladeskabsystem/LogFile.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using Ladeskabsystem.Interfaces;
 
-namespace LogFile
+namespace Ladeskabsystem
 {
-    public class LogFile
+    public class LogFile : ILogFile
     {
-        public void LogDoorLocked()
+        private string Logfile = "Logfile.txt";
+        public void WriteToLogLocked(int id)
         {
 
+            using (var writer = File.AppendText(Logfile))
+            {
+                writer.WriteLine(DateTime.Now + ": Skab låst med RFID: {0}", id);
+            }
         }
-        public void LogDoorUnLocked()
-        {
 
+        public void WriteToLogUnlocked(int id)
+        {
+            using (var writer = File.AppendText(Logfile))
+            {
+                writer.WriteLine(DateTime.Now + ": Skab låst op med RFID: {0}", id);
+            }
         }
     }
-
-
     
 }
